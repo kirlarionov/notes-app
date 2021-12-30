17f208612e8e1f06d5e2a35aa1c9e963382e9152
Kiril Larionov <kirkeriil@gmail.com>
2021-12-30 11:55:27 +0300

    new update

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,4 +1,5 @@
-import { todosReducer } from './todos'
 import { combineReducers } from 'redux'
+import { todosReducer } from './todos'
+
 
 export const rootReducer = combineReducers({ todos: todosReducer })
